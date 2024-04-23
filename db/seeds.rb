1154a9bb70c7bc099822904d7c144fb19fd5e17f
McNeil McCarley <mcneilmccarley@yahoo.com>
2024-04-22 23:01:51 -0500

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.create(name: "Kobe 5", price: 249.99, description: "Basketball shoes")
+Product.create(name: "IPhone 15", price: 1499.99, description: "Celluar Device")
+Product.create(name: "Strawberry Fanta", price: 2.99, description: "Greatest drink ever created")
